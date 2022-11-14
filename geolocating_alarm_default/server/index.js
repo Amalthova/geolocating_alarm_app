f4a8d58875518d3fbd802956673ed03f3134cb34
Azu <azuping@gmail.com>
2022-11-14 11:25:43 +0900

    chore : Error checking in index.js.  Should run migrations before listening.

--- geolocating_alarm_default/server/index.js
+++ geolocating_alarm_default/server/index.js
@@ -1,9 +1,16 @@
 const setupServer = require("./server");
-
+const db = require('../db/knex');
 const PORT = process.env.PORT || 4000;
-
 const server = setupServer();
-server.listen(PORT, ()=>{
-  console.log(`App is listening at localhost${PORT}`)
-});
 
+(async() => {
+  try {
+    await db.migrate.latest();
+    server.listen(PORT, ()=>{
+      console.log(`App is listening at localhost${PORT}`)
+    });
+  } catch(err) {
+    console.error(err);
+  }
+
+})()
