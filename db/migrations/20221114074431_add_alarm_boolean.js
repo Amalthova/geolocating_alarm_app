e9f7d0984e40d91110a26d8925fb594299f777c7
Azu <azuping@gmail.com>
2022-11-14 16:48:19 +0900

    Fix : New MIgration to add missing "Alarm Status" column to alarm database.

---
+++ db/migrations/20221114074431_add_alarm_boolean.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('alarms', table => {
+    table.boolean('alarm_status')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
