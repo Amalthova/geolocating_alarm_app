946efd7fef8820d15e26e41798b6054bfb75beb4
Azu <azuping@gmail.com>
2022-11-14 16:24:04 +0900

    Feat : Can now dynamically display alarm names

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [alarms, setAlarms] = useState([]);
+  const alarmList = alarms.map(oneAlarm => {
+    return <p>{ oneAlarm.alarm_name }</p>
+  })
+  
+  
+  useEffect(() => {
+    // fetch(alarms)
+    getAlarms();
+  }, [])
+
+  async function newAlarm() {
+
+  }
+
+  async function getAlarms() {
+    const retrievedAlarms = await axios.get('/api/alarms');
+    setAlarms(retrievedAlarms.data);
+  }
   return (
     <div>
       <h1>yeeee</h1>
+      { alarmList }
     </div>
   );
 }
