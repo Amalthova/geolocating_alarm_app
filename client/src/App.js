9bd88c05ac637767065f98908e0e805502aa68d7
Azu <azuping@gmail.com>
2022-11-15 09:26:49 +0900

    Working date/time display that refreshes every second.  It does jump a second every once in a while.

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,54 @@ import './App.css';
 
 function App() {
   const [alarms, setAlarms] = useState([]);
+  const [dateNow, setDate] = useState(new Date());
+
   const alarmList = alarms.map(oneAlarm => {
     return <p>{ oneAlarm.alarm_name }</p>
   })
   
   
   useEffect(() => {
-    // fetch(alarms)
+    //Create clock that updates
+    const currentDateAndTime = setInterval(() => {
+      setDate(new Date()); 
+    }, 1 * 1000);
+    return () => {
+      clearInterval(currentDateAndTime);
+    };
+      // let timeInMilliseconds = Date.now()
+      // let minute = 1000 * 60;
+      // let hour = minute * 60;
+      // let day = hour * 24;
+      // let year = day * 365 //Leap years????
     getAlarms();
   }, [])
-
   async function newAlarm() {
-
   }
 
   async function getAlarms() {
     const retrievedAlarms = await axios.get('/api/alarms');
     setAlarms(retrievedAlarms.data);
   }
+
+  class Board extends react.component {
+    renderSquare(i) {
+      return <Square value={i} />
+    }
+  }
+
   return (
     <div>
       <h1>yeeee</h1>
       { alarmList }
+      <p>{`${ dateNow }`}</p>
+      <form>
+        <input/>
+        <button className="square" onClick={function() { console.log('click'); }}>
+          {this.props.value}
+        </button>
+        <button> Monday </button>
+      </form>
     </div>
   );
 }
