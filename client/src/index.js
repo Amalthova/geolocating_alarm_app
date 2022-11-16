c7c296d950ae98b33d7421b402a50da76535c1de
Azu <azuping@gmail.com>
2022-11-16 13:50:03 +0900

    Fix : Change to axios URL handler. Should allow use of server side .env file for requests.

--- client/src/index.js
+++ client/src/index.js
@@ -5,27 +5,15 @@ import App from './components/App.js';
 import axios from 'axios';
 
 
-// axios.defaults.baseURL = `http://localhost:${process.env.PORT}/`; //, 'http://localhost:10000/'
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:3080"; //, 'http://localhost:10000/'
 // axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 // axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-axios.interceptors.request.use(request => {
-    console.log(request);
-    // Edit request config
-    return request;
-}, error => {
-    console.log(error);
-    return Promise.reject(error);
-});
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3080",
+// });
 
-axios.interceptors.response.use(response => {
-    console.log(response);
-    // Edit response config
-    return response;
-}, error => {
-    console.log(error);
-    return Promise.reject(error);
-});
+// export default api;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
